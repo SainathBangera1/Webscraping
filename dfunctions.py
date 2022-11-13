641d1b254b38793d8cc57cfa38e9601e91168c42
SainathBangera1 <psainathbangera@gmail.com>
2022-11-13 16:54:18 +0530

    pair.py added

--- dfunctions.py
+++ dfunctions.py
@@ -48,3 +48,55 @@ def duplicate(test_list):
         else:
             res.append(idx)
     return res
+
+
+def pairUp(data, i):
+    pair1 = []
+
+    pair_New = []
+    for x in range(len(data)):
+        temp_arr = []
+        if (x+1 < len(data)):
+            pair = set(data[i]) & set(data[x+1])
+            if (len(list(pair)) > 1):
+                temp_arr.append(pair)
+        if (len(temp_arr) != 0):
+            pair1.append(temp_arr)
+        temp_arr = []
+
+    for y in pair1:
+        if (len(list(y[0])) > 2):
+            pair_New.append(list(y[0]))
+
+    return pair_New
+
+
+# compare list helps to find the number paired numbers frequency by comparing big and small array
+def compareList(arr1, arr2):
+    count = 0
+    freq = 0
+    if (len(arr1) > len(arr2)):
+        for i in range(len(arr1)):
+            for m in arr2:
+                if (m in arr1[i]):
+                    count += 1
+                if (count >= 3):
+                    freq += 1
+                    count = 0
+    elif (len(arr2) > len(arr1)):
+        for i in range(len(arr2)):
+            for m in arr1:
+                if (m in arr2[i]):
+                    count += 1
+                if (count >= 3):
+                    freq += 1
+                    count = 0
+    return freq
+
+
+def pairTostring(arr):
+    res = ''
+    for i in arr:
+        res += str(i)+'-'
+    res = res[:-1]
+    return res
