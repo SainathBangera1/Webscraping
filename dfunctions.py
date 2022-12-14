e1d687c963bfbe0c844cce199bc967cb8ffe58a0
SainathBangera1 <psainathbangera@gmail.com>
2022-12-14 18:41:16 +0530

    Updated oddeven.py

--- dfunctions.py
+++ dfunctions.py
@@ -296,15 +296,34 @@ def arr_string(arr):
     res = str(arr[0])+'-'+str(arr[1])+'-'+str(arr[2])+'-'+str(arr[3])+'-'+str(arr[4])+'-'+str(arr[5])
     return res
 
-# odd=[]
-# even=[]
-# for i in range(1,91):
-#     if(i%2==0):
-#         even.append(i)
-#     else:
-#         odd.append(i)
+def spitODDEVEN(num):
+    odd=[]
+    even=[]
+    res=[]
+    for i in range(1,num+1):
+        if(i%2==0):
+            even.append(i)
+        else:
+            odd.append(i)
+    res.append(odd)
+    res.append(even)
+    return res
+# print(spitODDEVEN(45))
+#input = 45
+#output = [
+# [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45], 
+# [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44]
+# ]
 
-# print(odd)
-# print(f"Length of Odd Number: {len(odd)}")
-# print(even)
-# print(f"Length of Even Number: {len(even)}")
+def spitPrime(num):
+    res=[]
+    prime=[]
+    nonPrime=[]
+    for i in range(1,num+1):
+        if(prime(i)==0):
+            nonPrime.append(i)
+        else:
+            prime.append(i)
+    res.append(prime)
+    res.append(nonPrime)
+    return res
--- oddEven.py
+++ oddEven.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 import numpy as np
 import csv
-from dfunctions import date_Formater,date_Day
+from dfunctions import date_Formater,date_Day,spitODDEVEN,duplicate,arr_string,spitPrime,prime
 
 data = pd.read_csv("dataset.csv")
 data = data[["Date","1st Digit","2nd Digit","3rd Digit","4th Digit","5th Digit","6th Digit"]]
@@ -14,21 +14,29 @@ oddEven_DATA = []
 for i in data_Array:
     odd_Count=0
     even_Count=0
+    primeCount=0
+    nonPrimeCount=0
     tmp_arr=[]
     for y in range(1,len(i)):
         if(i[y]%2==0):
             even_Count+=1
         else:
             odd_Count+=1
-    tmp_arr=[date_Formater(i[0]),i[1],i[2],i[3],i[4],i[5],i[6],odd_Count,even_Count,(odd_Count/6)*100,(even_Count/6)*100]
+    for z in range(1,len(i)):
+        if(prime(i[z])==0):
+            primeCount+=1
+        else:
+            nonPrimeCount+=1
+    tmp_arr=[date_Formater(i[0]),i[1],i[2],i[3],i[4],i[5],i[6],odd_Count,even_Count,primeCount,nonPrimeCount,round((odd_Count/6)*100),round((even_Count/6)*100),round((primeCount/6)*100),round((nonPrimeCount/6)*100)]
     oddEven_DATA.append(tmp_arr)
     odd_Count=0
     even_Count=0
     tmp_arr=[]
 
+
 with open("oddEven.csv","w+") as file:
     myFile = csv.writer(file)
-    myFile.writerow(["Date","1st_D","2nd_D","3rd_D","4th_D","5th_D","6th_D","Odd","Even","% ODD","% /EVEN"])
+    myFile.writerow(["Date","1st_D","2nd_D","3rd_D","4th_D","5th_D","6th_D","Odd","Even","Prime","Non_Prime","% ODD","% EVEN","% Prime","% NonPrime"])
     for i in oddEven_DATA:
         date=i[0].split('/')
         if(date_Day(date[2],date[1],date[0])=="Tuesday"):
@@ -38,3 +46,42 @@ with open("oddEven.csv","w+") as file:
         elif(date_Day(date[2],date[1],date[0])=="Saturday"):
             myFile.writerow(i)
 print("Done writing oddEven.csv File.....")
+
+
+#1st{1 to 14} 2nd{15 to 27} 3rd {28 to 40} 4th {41 to 53} 5th {54 to 66} 6th {67 to 90} 
+
+oddEven_Array = spitODDEVEN(90)
+
+num = [random.choice(oddEven_Array[0]), random.choice(oddEven_Array[0]), random.choice(
+    oddEven_Array[0]), random.choice(oddEven_Array[1]), random.choice(oddEven_Array[1]), random.choice(oddEven_Array[1])]
+
+
+dupli = duplicate(num)
+
+
+while (len(dupli) > 0):
+    for i in dupli:
+        if (i == 0):
+            num[i] = random.choice(oddEven_Array[0])
+        elif (i == 1):
+            num[i] = random.choice(oddEven_Array[0])
+        elif (i == 2):
+            num[i] = random.choice(oddEven_Array[0])
+        elif (i == 3):
+            num[i] = random.choice(oddEven_Array[1])
+        elif (i == 4):
+            num[i] = random.choice(oddEven_Array[1])
+        elif (i == 5):
+            num[i] = random.choice(oddEven_Array[1])
+    dupli = duplicate(num)
+num.sort()
+
+array_toString = arr_string(num)
+
+with open("checkNum.csv","a+") as file:
+    myFile = csv.writer(file)
+    myFile.writerow([array_toString])
+    print("New Number inserted in check")
+
+print(f"Stock Numbers : {num[0]},{num[1]},{num[2]},{num[3]},{num[4]},{num[5]}")
+
